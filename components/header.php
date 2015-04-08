056d7b2992750f5656c08a4f3c47f9a99f935c52
Nick Hughes <NickH10@gmail.com>
2015-04-08 13:31:33 -0700

    added in basic files and set up directory structure since first commit didnt work

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Title</title>
+	<link type='text/css' rel='stylesheet' href='/css/header.css' />
+</head>
+<body>
+
+	<header>
+		This is my header
+	</header>
